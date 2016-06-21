7b873b24b3802fe8fc21fc3203f9c78af296ee54
N0rr <xt12pabeh@mail.ru>
2016-06-21 22:05:13 +0300

    build

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,46 @@
 "use strict";
 
 var gulp = require("gulp");
-var less = require("gulp-less");
+var del = require("del");
+var rename = require("gulp-rename");
 var plumber = require("gulp-plumber");
+var less = require("gulp-less");
+var minify = require("gulp-csso");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var mqpacker = require("css-mqpacker");
+var svgstore = require("gulp-svgstore");
+var svgmin = require("gulp-imagemin");
+var imagemin = require("gulp-imagemin");
 var server = require("browser-sync");
+var run = require("run-sequence");
+
+gulp.task("build", function(fn) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    "images",
+    "symbols",
+    fn
+  );
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("copy", function() {
+  return gulp.src ([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
 
 gulp.task("style", function() {
   gulp.src("less/style.less")
@@ -18,18 +53,41 @@ gulp.task("style", function() {
         "last 2 Firefox versions",
         "last 2 Opera versions",
         "last 2 Edge versions"
-      ]})
+      ]}),
+      mqpacker({
+        sort: false
+      })
     ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(gulp.dest("css"))
     .pipe(server.reload({stream: true}));
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("images", function() {
+  return gulp.src("build/img/**/*.{png,jpg,gif}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true})
+  ]))
+  .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("symbols", function() {
+  return gulp.src("build/img/icons/*.svg")
+    .pipe(svgmin())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("symbols.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("serve", function() {
   server.init({
-    server: ".",
-    notify: false,
-    open: true,
-    ui: false
+    server: "build"    
   });
 
   gulp.watch("less/**/*.less", ["style"]);
