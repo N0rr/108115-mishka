6fd3ce93858cf0358f62dd447ae877504861e3a1
N0rr <xt12pabeh@mail.ru>
2016-06-21 23:10:38 +0300

    build

--- gulpfile.js
+++ gulpfile.js
@@ -60,10 +60,10 @@ gulp.task("style", function() {
       })
     ]))
     .pipe(gulp.dest("build/css"))
+    .pipe(gulp.dest("css"))
     .pipe(minify())
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("build/css"))
-    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest("build/css"))    
     .pipe(server.reload({stream: true}));
 });
 
